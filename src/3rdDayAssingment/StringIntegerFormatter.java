3a56811751b279fcbc851696dda3c23fdce2ff04
ayush <ayushmangiri@gmail.com>
2025-02-01 23:30:53 +0530

    01-02-2025

---
+++ src/3rdDayAssingment/StringIntegerFormatter.java
@@ -0,0 +1,11 @@
+public class StringIntegerFormatter {
+    public static String formatOutput(String str, int num1, int num2) {
+        String concatenatedString = str.toLowerCase().replaceAll("[,\\s]", ""); // Remove spaces and commas
+        int sum = num1 + num2; // Sum the two integers
+        return concatenatedString + ", " + sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(formatOutput("Hello, Ayushman", 201, 1)); // Output: helloworld, 5
+    }
+}
